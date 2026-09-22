f970e5c11ce386e89aca771485d8ae29c8d9c0e8
Rielly Luiza <rielly.00000854571@unicap.br>
2026-09-22 14:16:01 +0000

    add animated background and adjust styled button call

--- portfolio/app/components/Hero.tsx
+++ portfolio/app/components/Hero.tsx
@@ -3,11 +3,12 @@
 import styled from "@emotion/styled";
 import Avatar from "../../public/avatar.jpg"
 import Image from "next/image";
-import { Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
 import theme from "../theme";
 import StyledButton from "./StyledButton";
+import { AnimatedBackground } from "./AnimatedBackground";
 
 export default function Hero() {
   
@@ -16,41 +17,46 @@ export default function Hero() {
     backgroundColor: theme.palette.primary.main,
     display: "flex",
     alignItems: "center",
-    justifyContent: "center",
   }))
 
   const StyledImg = styled(Image)(() => ({
-    width: "100%",
-    borderRadius: "50%"
+    width: "80%",
+    borderRadius: "50%",
+    border: `1px solid ${theme.palette.primary.contrastText}`,
   }))
 
   return (
     <>
       <StyledHero>
         <Container maxWidth="lg">
+          <Box sx={{ position: "relative" }}>
+            <Box sx={{ position: "absolute", width: "150%", top: -400, right: 0 }}>
+              <AnimatedBackground />
+            </Box>
+          </Box>
           <Grid container spacing={2}>
-            <Grid size={4}>
-              <StyledImg src={Avatar} alt=""/>
+            <Grid size={{ xs: 12, md: 5 }}>
+              <Box sx={{ position: "relative" }}>
+                <Box sx={{ position: "absolute", textAlign: "center", width: "100%" }}>
+                  <StyledImg src={Avatar} alt=""/>
+                </Box>
+              </Box>
             </Grid>
-            <Grid size={8}>
+            <Grid size={{ xs: 12, md: 7 }}>
               <Typography variant="h1" sx={{ textAlign: "center", color:"primary.contrastText" }}>Rielly Duarte</Typography>
-              <Typography variant="h2" sx={{ textAlign: "center", color:"primary.contrastText" }}>I'm a Computer Science student</Typography>
+              <Typography variant="h4" sx={{ textAlign: "center", color:"primary.contrastText" }}>I'm a Computer Science student</Typography>
               <Grid container sx={{display: "flex", justifyContent:"center"}} spacing={3}>
                 <Grid size={4} sx={{display: "flex", justifyContent:"center"}}>
-                  {StyledButton(<>
-                      <DownloadIcon />
-                      <Typography>
-                        Download CV
-                      </Typography>
-                  </>,)} 
+                  <StyledButton>
+                    <DownloadIcon />
+                    <Typography>Download CV</Typography>
+                  </StyledButton>
                 </Grid>
                 <Grid size={4} sx={{display: "flex", justifyContent:"center"}}>
-                  {StyledButton(<>
-                    <EmailIcon/>
-                    <Typography>
-                      Reach me
-                    </Typography>
-                  </>,)} 
+                  <StyledButton>
+                    <EmailIcon />
+                    <Typography>Reach Me</Typography>
+                  </StyledButton>
                 </Grid>
               </Grid>
             </Grid>
--- portfolio/app/components/StyledButton.tsx
+++ portfolio/app/components/StyledButton.tsx
@@ -1,22 +1,23 @@
 import { styled } from "@mui/material/styles";
+import { ReactNode } from "react";
 
-export default function CustomButton(children: React.ReactNode) {
-    const StyledButton = styled("button")(({ theme }) => ({
-        backgroundColor: "transparent",
-        border: `1px solid ${theme.palette.primary.contrastText}`,
-        borderRadius: "3px",
-        padding: "5px 15px",
-        width: "100%",
-        display: "inline-flex",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "10px",
-        color: theme.palette.primary.contrastText,
-        "&:hover": {
-            backgroundColor: theme.palette.secondary.light,
-            color: theme.palette.primary.main,
-        },
-    }));
+const StyledButtonBase = styled("button")(({ theme }) => ({
+    backgroundColor: "transparent",
+    border: `1px solid ${theme.palette.primary.contrastText}`,
+    borderRadius: "3px",
+    padding: "5px 15px",
+    width: "100%",
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "10px",
+    color: theme.palette.primary.contrastText,
+    "&:hover": {
+        backgroundColor: theme.palette.secondary.light,
+        color: theme.palette.primary.main,
+    },
+}));
 
-    return <StyledButton>{children}</StyledButton>;
+export default function StyledButton({ children }: { children: ReactNode }) {
+  return <StyledButtonBase>{children}</StyledButtonBase>;
 }
