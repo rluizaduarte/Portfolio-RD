f09e3c5ea2d210aa03e5bce7c507fcf8edc114f1
Rielly Luiza <rielly.00000854571@unicap.br>
2026-09-22 19:02:06 +0000

    simplify styled button (new animation)

--- portfolio/app/components/StyledButton.tsx
+++ portfolio/app/components/StyledButton.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 
 const StyledButtonBase = styled("button")(({ theme }) => ({
   position: "relative",
-  overflow: "hidden",
   backgroundColor: "transparent",
   border: `1px solid ${theme.palette.primary.contrastText}`,
   borderRadius: "3px",
@@ -15,21 +14,10 @@ const StyledButtonBase = styled("button")(({ theme }) => ({
   gap: "10px",
   color: theme.palette.primary.contrastText,
   cursor: "pointer",
-  zIndex: 0,
-  "&::before": {
-    content: '""',
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: theme.palette.secondary.main,
-    transform: "translateX(-100%)",
-    transition: "transform 0.3s ease",
-    zIndex: -1,
-  },
-  "&:hover::before": {
-    transform: "translateX(0)",
+  transition: "border-color 0.25s ease, color 0.25s ease",
+  "&:hover": {
+    borderColor: theme.palette.secondary.light,
+    color: theme.palette.secondary.main,
   },
 }));
 
